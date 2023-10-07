8c94227a5312fbc255efda2da3ab5767061c2ddb
ProGamerOO6 <andreidretcanu7@gmail.com>
2023-10-07 09:50:17 +0300

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -46,12 +46,12 @@ class MainWin(QWidget):
     def next_click(self):
         '''in aceasta functie trebuie sa instantiem (cream) fereastra a2-a'''
         '''numele clasei care creaza feastra a2-a o gasiti in fisierul secon_win.py'''
-        #self.tw = NumeleClaseiFerestreiDoi()
+        self.tw = TestWin()
         self.hide() #aceasta instructiune ascunde fereastra actuala pt a face loc pt cea noua
     
     def connects(self):
         '''functia care conecteaza butonul de functia next_click'''
-        #self.btn_next.clicked.connect(self.NUMELE_FUNCTIEI_CARE_AFISEAZA_FEREASTRA_DOI)
+        self.btn_next.clicked.connect(self.next_click)
 
     def set_appear(self):
         '''functia care seteaza aparenta ferestrei (titlu, dimensiune, pozitia pe ecran)'''
